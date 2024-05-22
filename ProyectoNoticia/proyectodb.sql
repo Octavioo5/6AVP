4c17a91c4a855f049169749be85d5442c97ad92e
Octavioo5 <124410741+Octavioo5@users.noreply.github.com>
2024-05-21 19:27:48 -0700

    Add files via upload

--- ProyectoNoticia/proyectodb.sql
+++ ProyectoNoticia/proyectodb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-05-2024 a las 04:28:45
+-- Tiempo de generación: 22-05-2024 a las 04:25:38
 -- Versión del servidor: 10.1.39-MariaDB
 -- Versión de PHP: 7.3.5
 
@@ -34,8 +34,16 @@ CREATE TABLE `noticias` (
   `lugar` varchar(200) NOT NULL,
   `fecha` date NOT NULL,
   `cuerpo` varchar(600) NOT NULL,
-  `imagenes` longblob NOT NULL
+  `imagenes` blob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `noticias`
+--
+
+INSERT INTO `noticias` (`id`, `nombre`, `lugar`, `fecha`, `cuerpo`, `imagenes`) VALUES
+(1, 'Tetalovers ganan', 'Sinaloa, MÃ©xico', '2024-05-21', 'Tetalovers ganan a cachondos', 0x656c4265736f2e6a7067),
+(2, 'Tetalovers pierden', 'Sinaloa, MÃ©xico', '0000-00-00', 'Tetalovers pierden la semi', 0x756c74696d6143656e612e6a7067);
 
 --
 -- Índices para tablas volcadas
@@ -55,7 +63,7 @@ ALTER TABLE `noticias`
 -- AUTO_INCREMENT de la tabla `noticias`
 --
 ALTER TABLE `noticias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
